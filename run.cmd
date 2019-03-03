ac267243f450e95e50a7b35ec4cba8a64c7c8d4d
neoandrey@yahoo.com <neoandrey@yahoo.com>
2019-03-03 06:00:54 +0100

    Address proxy problem with run script

--- run.cmd
+++ run.cmd
@@ -1,14 +1,20 @@
- @echo off
- set proxy_filepath=%cd%\proxy.txt
+@echo off
+setlocal enabledelayedexpansion
+set proxy_filepath=%cd%\proxy.txt
+ 
  echo "Checking proxy path: %proxy_filepath%"
  IF EXIST "%proxy_filepath%" (
    SET /p proxy_url=<"%proxy_filepath%"
  )  ELSE (
+ 
    echo "Please type proxy URL:"
-      set /p proxy_url=    
+      set /p proxy_url=   
  )
+
+IF [%proxy_url%] == [] SET proxy_url=""
+ 
 echo  %proxy_url%>%cd%\proxy.txt
- echo "setting proxy as:%proxy_url%"
+echo "setting proxy as:%proxy_url%"
  SET  HTTP_PROXY=%proxy_url%
  SET  HTTPS_PROXY=%proxy_url%
 powershell  -File  %cd%\run.ps1
